346527384159f603ba7c3324d6bf15e776caea93
girby2025 <girby.it.25@gmail.com>
2025-08-24 23:21:58 +0200

    Initial commit: import existing local project

--- 02_while_loop.py
+++ 02_while_loop.py
@@ -1,1 +1,21 @@
+# Example 2 algorithm (while) —  3 Login attempts
+# Correct password
+correct_password = "admin123"
 
+
+# Number of attempts
+attempts = 0
+
+
+# Allow up to 3 attempts
+while attempts < 3:
+    password = input("Enter password: ")
+    if password == correct_password:
+        print("Access granted")
+        break
+    else:
+        print("Wrong password")
+        attempts += 1
+
+if attempts == 3:
+    print("Access denied")
